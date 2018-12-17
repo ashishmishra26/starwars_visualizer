f2f3691422e87aa2ad996439ecdcbbfa500a39bc
Ashish Mishra <ashish@fusioncharts.com>
2018-12-17 11:59:53 +0530

    changed data fetching api

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-loop-func */
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios'
-import PageHeader from './components/PageHeader'
+// import PageHeader from './components/PageHeader'
 import Content from './components/Content'
 
 class App extends Component {
@@ -16,7 +15,6 @@ class App extends Component {
     return (
       <div className="App container-fluid">
         <div className="row">
-            {/* <PageHeader /> */}
             <Content films={this.state.films}/>
         </div>  
       </div>
@@ -28,17 +26,26 @@ class App extends Component {
   }
 
   fetchData () {
-    axios.get('https://swapi.co/api/films')
+    var config = {
+      headers: {'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+      'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+      'Access-Control-Allow-Credentials': true
+    }
+    };
+    fetch('https://www.swapi.co/api/films')
+    .then(response=>response.json())
     .then(response => {
-      let films = response.data.results,
+      console.log(response);
+      let films = response.results,
       updatedFilms = films;
       films.forEach(function(film, indexOfFilm) {
         for (let key in film) {
           if (film[key] instanceof Array) {
             let arrayProperty = film[key];
             arrayProperty.forEach(function(valueOfProp, indexOfProp){
-              axios.get(valueOfProp).then(function(result){
-                updatedFilms[indexOfFilm][key][indexOfProp] = result.data;
+              fetch(valueOfProp).then(response=>response.json()).then(function(result){
+                updatedFilms[indexOfFilm][key][indexOfProp] = result;
               });
             });
           }
