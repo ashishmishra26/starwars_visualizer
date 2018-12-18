9c750f1d92802c1f0f193a0e321e6cf91fa27246
Ashish Mishra <ashish@fusioncharts.com>
2018-12-18 16:15:44 +0530

    code documentation improved

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -5,6 +5,7 @@ import RightContent from './RightContent'
 class Content extends Component {
   render() {
     let {films, showList, movieIndex} = this.props;
+
     return (
       <div className="content col-12 row no-gutters">
         <Description films={films} showList={showList} movieIndex={movieIndex}/>
@@ -12,11 +13,17 @@ class Content extends Component {
       </div>
     )
   }
+  /**
+   * cardclickhandler calls toggleListVisibility which disable list view and show movie detail
+   */
   cardClickHandler = (index) => {
     this.props.toggleListVisibility(0, index.index);
   }
+  /**
+   * backclickhandler calls toggleListVisibility which enables list view
+   */
   backClickHandler = (visibility) => {
-    this.props.toggleListVisibility(visibility, undefined);
+    this.props.toggleListVisibility(visibility);
   }
 }
 
