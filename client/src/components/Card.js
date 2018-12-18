06fa96b17f3960070bb25bee8599c686a3c683a5
Ashish Mishra <ashish@fusioncharts.com>
2018-12-18 22:13:27 +0530

    added hover effect on card component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,14 +3,14 @@ import './css/card.css';
 
 export default class Card extends Component {
   render() {
-    let {title, content, index, height, width, extraClass, disableClick, styleForHeader, styleForContent, extraClassForHeader, extraClassForContent} = this.props, //these props are provided used to make card configurable. 
+    let {title, content, index, height, width, extraClass, disableClick, enableHover, styleForHeader, styleForContent, extraClassForHeader, extraClassForContent} = this.props, //these props are provided used to make card configurable. 
     cardDimention = {
         height: height,
         width: width
     };
     // card component has two parts header and content
     return (
-      <div style={cardDimention} className={`${!width && 'col-6'} ${!height && 'default-card-height'} ${extraClass} ${!disableClick && 'cursor'}`} onClick={() => {!disableClick && this.clickHandler({index})}}>
+      <div style={cardDimention} className={`${!width && 'col-6'} ${!height && 'default-card-height'} ${extraClass} ${!disableClick && 'cursor'}`} onClick={() => {!disableClick && this.clickHandler({index})}} onMouseOver={(e) => {enableHover && this.mouseOverHandler(e)}} onMouseOut={(e) => {enableHover && this.mouseOutHandler(e)}}>
         <div style={styleForHeader} className={`card-header ${extraClassForHeader}`}>{title}</div>
         <div style={styleForContent} className={`card-text ${extraClassForContent}`}>{content}</div>
       </div>
@@ -22,5 +22,11 @@ export default class Card extends Component {
    */
   clickHandler = (index) => {
     this.props.cardClickHandler(index);
+  }
+  mouseOverHandler = (e) => {
+    e.target.parentElement.style.transform = 'scale(1.1)';
+  }
+  mouseOutHandler = (e) => {
+    e.target.parentElement.style.transform = 'scale(1)';
   }
 }
--- client/src/components/RightContent.js
+++ client/src/components/RightContent.js
@@ -9,7 +9,7 @@ export default class RightContent extends Component {
     // otherwise movie detail component is shown with back button option
     return (
       <div className="right-content col-9 no-gutters">
-      { (showList) ? (films.results && films.results.map((film, index) => {return <Card key={index} index={index} title={film.title} content={`Release Date - ${film['release_date']}\n Director - ${film['director']}\n Producer - ${film['producer']}`} extraClass={'card-container col-6 ml-25'} cardClickHandler={cardClickHandler} styleForContent={{'whiteSpace': 'pre-wrap'}}/>})): ''}
+      { (showList) ? (films.results && films.results.map((film, index) => {return <Card key={index} index={index} title={film.title} content={`Release Date - ${film['release_date']}\n Director - ${film['director']}\n Producer - ${film['producer']}`} extraClass={'card-container col-6 ml-25'} cardClickHandler={cardClickHandler} styleForContent={{'whiteSpace': 'pre-wrap'}} enableHover={true}/>})): ''}
       { !showList && <button className="back-button" onClick={()=>{this.props.backClickHandler(1)}}>Back to list</button> }
       { !showList && <MovieDetail movieData={films.results[movieIndex]}/>}
       </div>
